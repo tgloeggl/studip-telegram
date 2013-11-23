304ad53c31b13395bb3d227efc1dcdb75a6811f3
Rasmus Fuhse <krasmus@gmail.com>
2013-11-23 19:13:34 +0100

    debugging

--- models/MailProcessor.class.php
+++ models/MailProcessor.class.php
@@ -4,12 +4,20 @@ require_once dirname(__file__)."/vendor/PlancakeEmailParser.php";
 
 class MailProcessor {
     
+    protected $mailaccount = "discussion";
+    protected $delimiter = "+";
+    protected $maildomain = null;
+    
     static public function getInstance() {
         $processor = new MailProcessor();
         return $processor;
     }
     
     public function __construct() {
+        $this->maildomain = $_SERVER['SERVER_NAME'];
+        if (strtolower(substr($this->maildomain, 0, 4)) === "www.") {
+            $this->maildomain = substr($this->maildomain, 4);
+        }
         //init configs
     }
     
@@ -21,8 +29,19 @@ class MailProcessor {
         $email_regular_expression='/^([-+.0-9=?A-Z_a-z{|}~])+@([-.0-9=?A-Z_a-z{|}~])+\.[a-zA-Z]{2,6}$/i';
         $mail = new PlancakeEmailParser($rawmail);
         $frommail = $mail->getHeader("From");
-        $frommail = preg_match($email_regular_expression, $frommail);
+        $output .= print_r($frommail);
+        preg_match($email_regular_expression, $frommail, $frommail);
+        $output .= print_r($frommail);
         $frommail = $frommail[0];
-        return $frommail;
+        
+        $recipients = $mail->getTo() + $mail->getCc();
+        foreach ($recipients as $recipient) {
+            $me = preg_match('/'.$this->mailaccount.'['.$this->delimiter.']([^@]+)@'.$this->maildomain.'/i', $recipient, $matches);
+            if ($me) {
+                $thread_id = $matches[1];
+            }
+        }
+        $thread = new BlubberPosting($thread_id);
+        return $output;
     }
 }
