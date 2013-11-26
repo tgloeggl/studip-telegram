c1d355d5d744621a62b1add784d8ac7420af172e
Rasmus Fuhse <krasmus@gmail.com>
2013-11-26 20:47:08 +0100

    debugging

--- models/MailProcessor.class.php
+++ models/MailProcessor.class.php
@@ -131,7 +131,7 @@ class MailProcessor {
             throw new AccessDeniedException("Emailadress not registered. Maybe you should try to send this with another email-adress?");
         }
         $body = $this->transformBody(studip_utf8decode(quoted_printable_decode($mail->getBody())));
-        StudipMail::sendMessage("ras@fuhse.org", "debugging", print_r($thread_id, true));
+        StudipMail::sendMessage("ras@fuhse.org", "debugging", print_r($this->mailaccount." | ".$this->delimiter." | ".$this->maildomain, true));
         if (!$thread->isNew() && $thread->isThread()) {
             //Rechtecheck TODO
             $check = false;
