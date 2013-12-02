b299b35e862b341e748a25dbd7ac6095882a9ea9
Rasmus Fuhse <krasmus@gmail.com>
2013-12-02 21:51:10 +0100

    bugfix unenabled plugin

--- PipeHere.cli.php
+++ PipeHere.cli.php
@@ -5,6 +5,18 @@ require_once dirname(__file__)."/../../../../cli/studip_cli_env.inc.php";
 require_once dirname(__file__)."/models/MailProcessor.class.php";
 
 PluginManager::getInstance()->getPlugins("SystemPlugin");
+$blubberMailEnabled = false;
+$plugin_infos = PluginManager::getInstance()->getPluginInfos("SystemPlugin");
+foreach ($plugin_infos as $plugin_info) {
+    if ((strtolower($plugin_info['class']) === "blubbermail") && $plugin_info['enabled']) {
+        $blubberMailEnabled = true;
+    }
+}
+
+if (!$blubberMailEnabled) {
+    echo "BlubberMail-Plugin ist nicht aktiviert. Nachrichten werden nicht als Blubber weitergestellt.";
+    exit(69);
+}
 
 $rawmail = file_get_contents('php://stdin');
 try {
